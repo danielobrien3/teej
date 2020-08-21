ec1e75bc50ea4f4a72d904f4a6bf82b67ef1099c
Danny O'Brien <dannyobrien@Dannys-Air.fios-router.home>
2020-08-21 13:20:24 -0400

    implements host context for handling dynamic css styles

--- src/app/splash-banner/splash-banner.component.ts
+++ src/app/splash-banner/splash-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Navigation_button } from '../navigation_button';
 
 @Component({
   selector: 'app-splash-banner',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash-banner.component.css']
 })
 export class SplashBannerComponent implements OnInit {
+
+  shop: Navigation_button = {
+    id: 1,
+    name: 'Shop',
+    link: 'placeholder'
+  };
+
+  customize: Navigation_button = {
+  	id: 2, 
+ 	name: 'Customize',
+ 	link: 'placeholder'
+  }
 
   constructor() { }
 
